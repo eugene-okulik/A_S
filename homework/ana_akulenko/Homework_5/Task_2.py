01c4ed0fabe0267b0040f9ad0744fa51caef689e
aakulenko <112194693+aakulenko@users.noreply.github.com>
2024-07-15 14:02:07 +0300

    Task_2.py

--- homework/ana_akulenko/Homework_5/Task_2.py
+++ homework/ana_akulenko/Homework_5/Task_2.py
@@ -1,7 +1,13 @@
 result = 'результат операции = 42 результат операции = 514 результат работы программы = 9'
+
 print(result.index('42'))
+
 print(result.index('514'))
+
 print(result.index('9'))
+
 print(int(result[21:23]) + 10)
+
 print(int(result[45:48]) + 10)
+
 print(int(result[78:80]) + 10)
