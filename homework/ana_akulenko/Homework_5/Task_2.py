5171b789e46403df08374d064638a7d020e3bafb
aakulenko <112194693+aakulenko@users.noreply.github.com>
2024-07-26 09:36:56 +0300

    aakulenko_5

---
+++ homework/ana_akulenko/Homework_5/Task_2.py
@@ -0,0 +1,17 @@
+result_1 = 'результат операции: 42'
+
+result_2 = 'результат операции: 514'
+
+result_3 = 'результат работы программы: 9'
+
+idx1 = result_1.index(': ')
+
+idx2 = result_2.index(': ')
+
+idx3 = result_3.index(': ')
+
+print(int(result_1[idx1 + 1: ]) + 10)
+
+print(int(result_2[idx2 + 1: ]) + 10)
+
+print(int(result_3[idx3 + 1: ]) + 10)
