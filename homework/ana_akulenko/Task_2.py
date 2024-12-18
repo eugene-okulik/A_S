9c4877d278096a596fcfa6ae87b30cd0d2aec564
1ca59141 <112194693+1ca59141@users.noreply.github.com>
2024-12-18 14:10:21 +0300

    Homework_8

---
+++ homework/ana_akulenko/Task_2.py
@@ -0,0 +1,24 @@
+import sys
+sys.set_int_max_str_digits(100000)
+import itertools
+
+
+def fib():
+    a = 0
+    b = 1
+    while True:
+        yield b
+        a, b = b, a + b
+
+
+index1, index2, index3, index4 = 5, 200, 1000, 10000
+
+lst = ([next(itertools.islice(fib(), index1, None)),
+
+next(itertools.islice(fib(), index2, None)),
+
+next(itertools.islice(fib(), index3, None)),
+
+next(itertools.islice(fib(), index4, None))])
+
+print(lst)
